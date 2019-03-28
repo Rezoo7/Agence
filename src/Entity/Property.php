78bffa4e4640be20eeebe96f314642c5e85af103
Maxime <maxime.guigourez@gmail.com>
2019-03-28 23:50:20 +0100

    affichage premier bien 3/16 fin

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
@@ -10,8 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 class Property
 {
     const HEAT = [
-        0 => 'electric',
-        1 => 'gaz'
+        0 => 'Electrique',
+        1 => 'Gaz'
     ];
 
     /**
@@ -108,6 +109,10 @@ class Property
         return $this;
     }
 
+    public function getSlug():string {
+        return (new Slugify())->slugify($this->title,"_");
+    }
+
     public function getDescription(): ?string
     {
         return $this->description;
@@ -173,6 +178,10 @@ class Property
         return $this->price;
     }
 
+    public function getFormattedPrice():string {
+        return number_format($this->price,0," "," ");
+    }
+
     public function setPrice(int $price): self
     {
         $this->price = $price;
@@ -190,6 +199,10 @@ class Property
         $this->heat = $heat;
 
         return $this;
+    }
+
+    public function getHeatType():string {
+        return self::HEAT[$this->heat];
     }
 
     public function getCity(): ?string
