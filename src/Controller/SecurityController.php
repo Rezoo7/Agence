1bf298528ef8e2498548139d31475918e32b754d
Maxime <maxime.guigourez@gmail.com>
2019-03-31 16:48:30 +0200

    Security - Connexion  6/16 14:00 adds

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     * @param AuthenticationUtils $au
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(AuthenticationUtils $au)
+    {
+        $lastusername = $au->getLastUsername();
+        $error = $au->getLastAuthenticationError();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastusername,
+            'error' => $error
+
+        ]);
+    }
+}
