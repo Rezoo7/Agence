8bc385895a809d1ae9993181ddbffe1a64aadf38
Maxime <maxime.guigourez@gmail.com>
2019-04-02 12:57:07 +0200

    Options -  options tri   9/16 21:00

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
@@ -18,6 +19,16 @@ class PropertySearch
      * @Assert\Range(min=10,max=400)
      */
     private $minSurface;
+
+    /**
+     * @var ArrayCollection
+     */
+    private $options;
+
+    public function __construct()
+    {
+        $this->options = new ArrayCollection();
+    }
 
     /**
      * @return int|null
@@ -54,6 +65,26 @@ class PropertySearch
         $this->minSurface = $minSurface;
         return $this;
     }
+
+    /**
+     * @return ArrayCollection
+     */
+    public function getOptions(): ArrayCollection
+    {
+        return $this->options;
+    }
+
+    /**
+     * @param ArrayCollection $options
+     * @return PropertySearch
+     */
+    public function setOptions(ArrayCollection $options): PropertySearch
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+
 
 
 }
